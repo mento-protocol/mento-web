8713c198745e2658478fd32ae45130e3ecef0ddb
J M Rossy <jm.rossy@gmail.com>
2021-11-14 19:00:35 +0100

    Set CSP headers in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
 const { version } = require('./package.json')
 
+const isDev = process.env.NODE_ENV !== 'production'
+
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'; script-src 'self'${
+      isDev ? " 'unsafe-eval'" : ''
+    }; connect-src 'self' https://*.celo.org https://*.celo-testnet.org wss://walletconnect.celo.org wss://relay.walletconnect.org; img-src 'self' data: https://raw.githubusercontent.com; style-src 'self' 'unsafe-inline'; font-src 'self' data:; base-uri 'self'; form-action 'self'`,
+  },
+]
+
 module.exports = {
   webpack: (config, { webpack }) => {
     config.resolve.fallback = {
@@ -18,6 +45,16 @@ module.exports = {
       {
         source: '/:any*',
         destination: '/',
+      },
+    ]
+  },
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
       },
     ]
   },
