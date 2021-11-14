934c0b0c12d0a64a4a8727f20a744be12732f03a
J M Rossy <jm.rossy@gmail.com>
2021-11-14 19:48:03 +0100

    Remove dead table code

--- next.config.js
+++ next.config.js
@@ -52,7 +52,6 @@ module.exports = {
   async headers() {
     return [
       {
-        // Apply these headers to all routes in your application.
         source: '/(.*)',
         headers: securityHeaders,
       },
