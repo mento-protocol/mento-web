854cf03d467f6043b9701671c5e9853000bcfcc0
J M Rossy <jm.rossy@gmail.com>
2023-04-13 11:51:09 +0900

    Add info banner about mento v2

--- src/components/nav/InfoBanner.tsx
+++ src/components/nav/InfoBanner.tsx
@@ -1,13 +1,13 @@
 export function InfoBanner() {
   return (
     <a
-      href="https://forum.celo.org/t/mento-core-contract-upgrade-1-multi-collateral-mint/4942"
+      href="https://forum.celo.org/t/towards-mento-2-0/3473"
       target="_blank"
       rel="noopener noreferrer"
       className="block py-1.5 w-full text-white text-center text-sm bg-green-800 hover:bg-green-900 active:bg-green-950 ring-1 ring-inset ring-green-700 transition-all duration-300"
     >
-      Exciting Mento upgrades are coming soon!{' '}
-      <span className="underline underline-offset-2">Learn more</span>
+      This app uses Mento version 2.{' '}
+      <span className="underline underline-offset-2">Learn more about Mento versions.</span>
     </a>
   )
 }
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react'
 import { Footer } from 'src/components/nav/Footer'
 import { Header } from 'src/components/nav/Header'
+import { InfoBanner } from 'src/components/nav/InfoBanner'
 import { PollingWorker } from 'src/features/polling/PollingWorker'
 import { HeadMeta } from 'src/layout/HeadMeta'
 
@@ -13,7 +14,7 @@ export function AppLayout({ pathName, children }: PropsWithChildren<Props>) {
     <>
       <HeadMeta pathName={pathName} />
       <div className="flex flex-col h-full min-h-screen w-full min-w-screen bg-gradient-linear">
-        {/* <InfoBanner /> */}
+        <InfoBanner />
         <Header pathName={pathName} />
         <main className="grow flex items-center justify-center">{children}</main>
         <Footer />
