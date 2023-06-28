664ac137ac116aa68ab7120d61a00dc70cbfe4e6
J M Rossy <jm.rossy@gmail.com>
2023-06-28 12:06:08 -0400

    Update info banner for deprecation notice

--- src/components/nav/InfoBanner.tsx
+++ src/components/nav/InfoBanner.tsx
@@ -1,13 +1,12 @@
 export function InfoBanner() {
   return (
     <a
-      href="https://forum.celo.org/t/mento-core-contract-upgrade-1-multi-collateral-mint/4942"
+      href="https://app.mento.org"
       target="_blank"
       rel="noopener noreferrer"
-      className="block py-1.5 w-full text-white text-center text-sm bg-green-800 hover:bg-green-900 active:bg-green-950 ring-1 ring-inset ring-green-700 transition-all duration-300"
+      className="block py-1.5 w-full text-white text-center text-sm bg-red-800 hover:bg-red-900 active:bg-red-950 ring-1 ring-inset ring-red-700 transition-all duration-300"
     >
-      Exciting Mento upgrades are coming soon!{' '}
-      <span className="underline underline-offset-2">Learn more</span>
+      This app is deprecated. Please use https://app.mento.org for the latest Mento features{' '}
     </a>
   )
 }
