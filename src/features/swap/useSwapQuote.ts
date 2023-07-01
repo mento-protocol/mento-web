112924ae4ecfba8af72611920e8d8aae8e4fafbe
J M Rossy <jm.rossy@gmail.com>
2023-07-01 18:34:19 -0400

    Remove dead code in useSwapQuote

--- src/features/swap/useSwapQuote.ts
+++ src/features/swap/useSwapQuote.ts
@@ -12,56 +12,6 @@ import { useDebounce } from 'src/utils/debounce'
 import { logger } from 'src/utils/logger'
 import { useChainId } from 'wagmi'
 
-// export function useSwapOutQuote(
-//   fromAmount: string | number,
-//   fromAmountWei: string,
-//   fromTokenId: TokenId,
-//   toTokenId: TokenId
-// ) {
-//   const chainId = useChainId()
-
-//   const debouncedFromAmountWei = useDebounce(fromAmountWei, 350)
-
-//   const { isLoading, isError, error, data } = useQuery(
-//     ['useSwapOutQuote', debouncedFromAmountWei, fromTokenId, toTokenId],
-//     async () => {
-//       const fromAmountBN = ethers.BigNumber.from(debouncedFromAmountWei)
-//       if (fromAmountBN.lte(0) || !fromTokenId || !toTokenId) return null
-//       const mento = await getMentoSdk(chainId)
-//       const fromTokenAddr = getTokenAddress(fromTokenId, chainId)
-//       const toTokenAddr = getTokenAddress(toTokenId, chainId)
-//       const toAmountWei = (
-//         await mento.getAmountOut(fromTokenAddr, toTokenAddr, fromAmountBN)
-//       ).toString()
-//       const toAmount = fromWeiRounded(toAmountWei, Tokens[toTokenId].decimals)
-//       return {
-//         toAmountWei,
-//         toAmount,
-//         rate: calcExchangeRate(fromAmount, toAmount),
-//       }
-//     },
-//     {
-//       staleTime: SWAP_QUOTE_REFETCH_INTERVAL,
-//       refetchInterval: SWAP_QUOTE_REFETCH_INTERVAL,
-//     }
-//   )
-
-//   useEffect(() => {
-//     if (error) {
-//       toast.error('Unable to fetch swap out amount')
-//       logger.error(error)
-//     }
-//   }, [error])
-
-//   return {
-//     isLoading,
-//     isError,
-//     toAmountWei: data?.toAmountWei || '0',
-//     toAmount: data?.toAmount || '0',
-//     rate: data?.rate,
-//   }
-// }
-
 export function useSwapQuote(
   amountWei: string,
   direction: SwapDirection,
