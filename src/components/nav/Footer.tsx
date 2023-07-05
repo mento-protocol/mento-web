6bd6916bbbad310ac221bb0215e756c104fc7073
boqdan <bogdan@bowd.io>
2023-07-05 11:53:10 +0200

    feat: add info banner and toy theme switcher

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -9,9 +9,11 @@ import { useAppSelector } from 'src/features/store/hooks'
 import Discord from 'src/images/logos/discord.svg'
 import Github from 'src/images/logos/github.svg'
 import Twitter from 'src/images/logos/twitter.svg'
+import { useDarkMode } from 'src/styles/mediaQueries'
 import { isStale } from 'src/utils/time'
 
 export function Footer() {
+  const { isDarkMode, setDarkMode } = useDarkMode()
   return (
     <footer className="relative z-10 w-screen py-4 px-7">
       <div className="flex justify-between items-center">
@@ -19,6 +21,10 @@ export function Footer() {
           <FooterIconLink to={links.twitter} imgSrc={Twitter} alt="Twitter" />
           <FooterIconLink to={links.github} imgSrc={Github} alt="Github" />
           <FooterIconLink to={links.discord} imgSrc={Discord} alt="Discord" />
+        </div>
+        <div className="flex items-center opacity-90">
+          Theme:
+          <input type="checkbox" checked={isDarkMode} onChange={() => setDarkMode(!isDarkMode)} />
         </div>
         <BlockIndicator />
       </div>
@@ -70,9 +76,8 @@ function BlockIndicator() {
       >
         <div className="mr-3 text-sm font-medium pt-px">{summary}</div>
         <div
-          className={`rounded-full w-3.5 h-3.5 ${'bg-' + classColor} border-2 ${
-            'border-' + classColor
-          } border-opacity-50`}
+          className={`rounded-full w-3.5 h-3.5 ${'bg-' + classColor} border-2 ${'border-' + classColor
+            } border-opacity-50`}
         ></div>
         <div className="hidden bg-yellow-300 bg-red-600"></div>
       </button>
--- src/components/nav/InfoBanner.tsx
+++ src/components/nav/InfoBanner.tsx
@@ -1,13 +1,19 @@
 export function InfoBanner() {
   return (
-    <a
-      href="https://forum.celo.org/t/towards-mento-2-0/3473"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="block py-1.5 w-full text-white text-center text-sm bg-green-800 hover:bg-green-900 active:bg-green-950 ring-1 ring-inset ring-green-700 transition-all duration-300"
-    >
-      This app uses Mento version 2.{' '}
-      <span className="underline underline-offset-2">Learn more about Mento versions.</span>
-    </a>
+    <div className="w-full h-6 py-1 bg-gray-950 dark:bg-neutral-800 justify-center items-center inline-flex">
+      <div className="justify-start items-start gap-1 inline-flex">
+        <div className="text-gray-400 dark:text-neutral-400 text-[12px] font-normal leading-none">
+          This app uses Mento version 2.
+        </div>
+        <a
+          href="https://forum.celo.org/t/towards-mento-2-0/3473"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white text-[12px] font-medium leading-none"
+        >
+          Learn more about Mento versions.
+        </a>
+      </div>
+    </div>
   )
 }
