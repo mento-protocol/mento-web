ffb1063e809066ea0f895b43c6478e3ba07cdbf6
Bayological <6872903+bayological@users.noreply.github.com>
2023-07-05 12:20:19 -0500

    feat: connect button dark mode styling

--- src/components/nav/ConnectButton.tsx
+++ src/components/nav/ConnectButton.tsx
@@ -6,9 +6,13 @@ import { Identicon } from 'src/components/Identicon'
 import { SolidButton } from 'src/components/buttons/SolidButton'
 import { BalancesSummary } from 'src/components/nav/BalancesSummary'
 import { NetworkModal } from 'src/components/nav/NetworkModal'
+import ClipboardDark from 'src/images/icons/clipboard-plus-dark.svg'
 import Clipboard from 'src/images/icons/clipboard-plus.svg'
+import CubeDark from 'src/images/icons/cube-dark.svg'
 import Cube from 'src/images/icons/cube.svg'
+import LogoutDark from 'src/images/icons/logout-dark.svg'
 import Logout from 'src/images/icons/logout.svg'
+import WalletDark from 'src/images/icons/wallet-dark.svg'
 import Wallet from 'src/images/icons/wallet.svg'
 import { DropdownModal } from 'src/layout/Dropdown'
 import { shortenAddress } from 'src/utils/addresses'
@@ -52,20 +56,27 @@ export function ConnectButton() {
 
               <div className={styles.menuOption} onClick={onClickCopy}>
                 <CopyIcon />
-                <div>Copy Address</div>
+                <div className="transition-colors duration-200 hover:text-gray-500 active:text-gray-200">
+                  Copy Address
+                </div>
               </div>
               <div className={styles.menuOption} onClick={onClickChangeNetwork}>
                 <NetworkIcon />
-                <div>Change Network</div>
+                <div className="transition-colors duration-200 hover:text-gray-500 active:text-gray-200">
+                  {' '}
+                  Change Network
+                </div>
               </div>
-              <hr className="mx-5 mt-4" />
+              <hr className="mx-5 mt-4 dark:border-[#333336]" />
               <div className={styles.menuOption} onClick={onClickDisconnect}>
                 <LogoutIcon />
-                <div>Disconnect</div>
+                <div className="transition-colors duration-200 dark:text-primary-blush hover:text-gray-500 active:text-gray-200">
+                  Disconnect
+                </div>
               </div>
             </div>
           )}
-          modalClasses="right-px min-w-[272px] border-[1px] border-solid border-black text-sm !rounded-[16px] !shadow-lg2"
+          modalClasses="right-px min-w-[272px] border-[1px] border-solid border-black dark:border-[#333336] text-sm !rounded-[16px] !shadow-lg2 dark:bg-[#1D1D20]/[1]"
         />
       ) : (
         <SolidButton
@@ -87,7 +98,8 @@ export function ConnectButton() {
 function WalletIcon() {
   return (
     <div className="flex items-center sm:mr-[12px]">
-      <Image src={Wallet} alt="" width={20} height={20} />
+      <Image className="inline dark:hidden" src={Wallet} alt="" width={20} height={20} />
+      <Image className="hidden dark:inline" src={WalletDark} alt="" width={20} height={20} />
     </div>
   )
 }
@@ -95,7 +107,8 @@ function WalletIcon() {
 function LogoutIcon() {
   return (
     <div className="flex items-center sm:mr-1.5">
-      <Image src={Logout} alt="" width={32} height={32} />
+      <Image className="inline dark:hidden" src={Logout} alt="" width={32} height={32} />
+      <Image className="hidden dark:inline" src={LogoutDark} alt="" width={32} height={32} />
     </div>
   )
 }
@@ -103,7 +116,8 @@ function LogoutIcon() {
 function NetworkIcon() {
   return (
     <div className="flex items-center sm:mr-1.5">
-      <Image src={Cube} alt="" width={32} height={32} />
+      <Image className="inline dark:hidden" src={Cube} alt="" width={32} height={32} />
+      <Image className="hidden dark:inline" src={CubeDark} alt="" width={32} height={32} />
     </div>
   )
 }
@@ -111,7 +125,8 @@ function NetworkIcon() {
 function CopyIcon() {
   return (
     <div className="flex items-center sm:mr-1.5">
-      <Image src={Clipboard} alt="" width={32} height={32} />
+      <Image className="inline dark:hidden" src={Clipboard} alt="" width={32} height={32} />
+      <Image className="hidden dark:inline" src={ClipboardDark} alt="" width={32} height={32} />
     </div>
   )
 }
@@ -119,9 +134,8 @@ function CopyIcon() {
 const styles = {
   // TODO DRY up with SolidButton styles
   walletButtonDefault:
-    'shadow-md h-[52px] min-w-[137px] py-[16px] !pl-[20px] !pr-[24px] sm:px-4 rounded-lg border-[1px] border-solid border-black font-medium leading-5',
+    'shadow-md h-[52px] min-w-[137px] py-[16px] !pl-[20px] !pr-[24px] sm:px-4 rounded-lg border-[1px] border-solid border-black dark:border-clean-white font-medium leading-5 dark:text-white dark:bg-primary-dark',
   walletButtonConnected:
-    'flex items-center justify-center bg-white text-black hover:bg-gray-100 active:bg-gray-200 rounded-full shadow-md transition-all duration-300', //TODO: @bayo hover shadow needs to be adjusted for padding
-  menuOption:
-    'flex items-center cursor-pointer rounded hover:bg-gray-100 active:bg-gray-200 pl-4 pt-4',
+    'flex items-center justify-center bg-white text-black rounded-full shadow-md transition-all duration-300', //TODO: @bayo hover shadow needs to be adjusted for padding
+  menuOption: 'flex items-center cursor-pointer rounded pl-4 pt-4 dark:text-white',
 }
