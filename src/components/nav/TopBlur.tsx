0ba1543822bc93616064ed0a1eb324f0b9d0d299
Bayological <6872903+bayological@users.noreply.github.com>
2023-07-10 14:09:45 -0500

    style: make tob blur mobile friendly

--- src/components/nav/TopBlur.tsx
+++ src/components/nav/TopBlur.tsx
@@ -3,8 +3,11 @@ import Blur from 'src/images/background/background_blur.svg'
 
 export function TopBlur() {
   return (
-    <div className="fixed z-0 w-screen transform -translate-x-1/2 left-1/2 top-6">
-      <Image src={Blur} alt="Blur" quality={100} className="min-h-[252px] w-screen" />
+    <div className="fixed top-0 z-0 w-screen">
+      <picture>
+        <source media="(min-width: 640px)" srcSet={Blur.src} />
+        <Image src={Blur} alt="Blur" quality={100} className="w-screen h-[150px] md:h-[252px]" />
+      </picture>
     </div>
   )
 }
