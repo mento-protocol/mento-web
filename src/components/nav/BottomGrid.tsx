9656b4d13527ba86512e6f2de027d66a3edd408b
Bayological <6872903+bayological@users.noreply.github.com>
2023-07-10 13:54:34 -0500

    style: use screen width for images

--- src/components/nav/BottomGrid.tsx
+++ src/components/nav/BottomGrid.tsx
@@ -4,18 +4,18 @@ import BackgroundTiles from 'src/images/background/background_tiles_light.png'
 
 export function BottomGrid() {
   return (
-    <div className="absolute bottom-0 z-10 transform -translate-x-1/2 left-1/2">
+    <div className="absolute bottom-0 z-10 w-screen transform -translate-x-1/2 left-1/2">
       <Image
         src={BackgroundTiles}
         alt="Background Tiles"
         quality={100}
-        className="inline dark:hidden min-h-[201px] min-w-[1270px]"
+        className="inline dark:hidden min-h-[201px] w-screen"
       />
       <Image
         src={BackgroundTilesDark}
         alt="Background Tiles"
         quality={100}
-        className="hidden dark:inline min-h-[201px] min-w-[1270px]"
+        className="hidden dark:inline min-h-[201px] w-screen"
       />
     </div>
   )
--- src/components/nav/TopBlur.tsx
+++ src/components/nav/TopBlur.tsx
@@ -3,8 +3,8 @@ import Blur from 'src/images/background/background_blur.svg'
 
 export function TopBlur() {
   return (
-    <div className="fixed z-0 transform -translate-x-1/2 left-1/2 top-6">
-      <Image src={Blur} alt="Blur" quality={100} className="min-h-[252px] min-w-[1440px]" />
+    <div className="fixed z-0 w-screen transform -translate-x-1/2 left-1/2 top-6">
+      <Image src={Blur} alt="Blur" quality={100} className="min-h-[252px] w-screen" />
     </div>
   )
 }
