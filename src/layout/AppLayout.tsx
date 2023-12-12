cec4c9358483529b508aca73bada68f214ab7c7e
Philip Paetz <philip.paetz@me.com>
2023-12-12 13:11:13 +0100

    feat: removed v2 banner

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -3,7 +3,6 @@ import { PropsWithChildren } from 'react'
 import { BottomGrid } from 'src/components/nav/BottomGrid'
 import { Footer } from 'src/components/nav/Footer'
 import { Header } from 'src/components/nav/Header'
-import { InfoBanner } from 'src/components/nav/InfoBanner'
 import { TopBlur } from 'src/components/nav/TopBlur'
 import { PollingWorker } from 'src/features/polling/PollingWorker'
 import { HeadMeta } from 'src/layout/HeadMeta'
@@ -24,7 +23,6 @@ export function AppLayout({ pathName, children }: PropsWithChildren<Props>) {
       <div
         className={`flex flex-col w-full h-full min-h-screen min-w-screen bg-white dark:bg-primary-dark font-inter ${foundersGrotesk.variable}`}
       >
-        <InfoBanner />
         <TopBlur />
         <Header />
         <main className={`relative z-20 flex items-center justify-center grow`}>{children}</main>
