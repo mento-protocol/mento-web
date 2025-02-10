7d72e511723816ce2b4a6ee00cc88faeda446c8b
Andrew718PLTS <andrew.polovets@mentolabs.xyz>
2025-02-10 18:56:58 +0100

    Fix: 'Balance still loading' state

--- src/features/swap/SwapForm.tsx
+++ src/features/swap/SwapForm.tsx
@@ -95,7 +95,7 @@ function SwapForm() {
         <SwapFormInputs balances={balances} />
         {showSlippage && <SlippageRow />}
         <div className="flex justify-center w-full my-6 mb-0">
-          <SubmitButton isWalletConnected={isWalletConnected} />
+          <SubmitButton isWalletConnected={isWalletConnected} isBalanceLoaded={isBalanceLoaded} />
         </div>
       </Form>
     </Formik>
@@ -291,7 +291,7 @@ function SlippageRow() {
   )
 }
 
-function SubmitButton({ isWalletConnected }: ISubmitButtonProps) {
+function SubmitButton({ isWalletConnected, isBalanceLoaded }: ISubmitButtonProps) {
   const { chain, chains } = useNetwork()
   const { switchNetworkAsync } = useSwitchNetwork()
   const { openConnectModal } = useConnectModal()
@@ -352,14 +352,15 @@ function SubmitButton({ isWalletConnected }: ISubmitButtonProps) {
   const buttonText = useMemo(() => {
     if (!isWalletConnected) return 'Connect Wallet'
     if (!isOnCelo) return 'Switch to Celo Network'
+    if (isWalletConnected && !isBalanceLoaded) return 'Balance still loading...'
     if (hasError) return errorText
     return 'Continue'
-  }, [errorText, hasError, isWalletConnected, isOnCelo])
+  }, [errorText, hasError, isWalletConnected, isOnCelo, isBalanceLoaded])
   const onClick = useMemo(() => {
     if (!isWalletConnected) return openConnectModal
     if (!isOnCelo) return switchToNetwork
     return undefined
-  }, [isWalletConnected, isOnCelo, openConnectModal, switchToNetwork])
+  }, [isWalletConnected, isBalanceLoaded, isOnCelo, openConnectModal, switchToNetwork])
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
@@ -370,6 +371,7 @@ function SubmitButton({ isWalletConnected }: ISubmitButtonProps) {
         onClick={onClick}
         type={buttonType}
         isWalletConnected={isWalletConnected}
+        isBalanceLoaded={isBalanceLoaded}
       >
         {buttonText}
       </Button3D>
