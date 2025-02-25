8154fd008a2a00368af25d38f40410398e0acdbf
Philip Paetz <philip.paetz@me.com>
2025-02-25 14:01:05 +0100

    chore: fix prettier error

--- src/images/tokens/TokenIcon.tsx
+++ src/images/tokens/TokenIcon.tsx
@@ -9,11 +9,11 @@ import axlEUROCIcon from 'src/images/tokens/axlEUROC.svg'
 import axlUSDCIcon from 'src/images/tokens/axlUSDC.svg'
 import cCOPIcon from 'src/images/tokens/cCOP.svg'
 import cEURIcon from 'src/images/tokens/cEUR.svg'
+import cGHSIcon from 'src/images/tokens/cGHS.svg'
 import cKESIcon from 'src/images/tokens/cKES.svg'
 import cREALIcon from 'src/images/tokens/cREAL.svg'
 import cUSDIcon from 'src/images/tokens/cUSD.svg'
 import eXOFIcon from 'src/images/tokens/eXOF.svg'
-import cGHSIcon from 'src/images/tokens/cGHS.svg'
 
 interface Props {
   token?: Token | null
